1d814f9909e698cc0c0be34d265dae803177ab1e
Jeremiah Lant <jeremiahlant@gmail.com>
2015-07-27 11:51:42 -0400

    completed geojson tutorial

--- javascript/hello-leaflet/geojson-with-leaflet/js/map.js
+++ javascript/hello-leaflet/geojson-with-leaflet/js/map.js
@@ -1,5 +1,6 @@
 var map = L.map('map').setView([39.74739, -105], 13);
 
+// add a tile layer from MapBox
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
@@ -12,38 +13,64 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 //     attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
 // }).addTo(map);
 
-var geojsonMarkerOptions = {
-    radius: 8,
-    fillColor: "#ff7800",
-    color: "#000",
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8
-};
+// add a tile layer from ESRI World Street Map
+// var Esri_WorldStreetMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+// 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+// });
+// Esri_WorldStreetMap.addTo(map);
 
-var geojsonFeature = {
-    "type": "Feature",
-    "properties": {
-        "name": "Coors Field",
-        "amenity": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!"
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.99404, 39.75621]
-    }
-};
+var baseballIcon = L.icon({
+    iconUrl: 'images/baseball-marker.png',
+    iconSize: [32, 37],
+    iconAnchor: [16, 37],
+    popupAnchor: [0, -28]
+});
 
 function onEachFeature(feature, layer) {
-    // does this feature have a property named popupContent?
+    var popupContent = "<p>I started out as a GeoJson " + feature.geometry.type + ", but now I am a Leaflet vector!</p>";
+
     if (feature.properties && feature.properties.popupContent) {
-        layer.bindPopup(feature.properties.popupContent);
+        popupContent += feature.properties.popupContent;
     }
+
+    layer.bindPopup(popupContent);
 }
 
-L.geoJson(geojsonFeature, {
+L.geoJson([bicycleRental, campus], {
+    style: function (feature) {
+        return feature.properties && feature.properties.style;
+    },
+
     onEachFeature: onEachFeature,
+
     pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, geojsonMarkerOptions);
+        return L.circleMarker(latlng, {
+            radius: 8,
+            fillColor: "#ff7800",
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        });
     }
+}).addTo(map);
+
+L.geoJson(freeBus, {
+    filter: function (feature, layer) {
+        if (feature.properties) {
+            // If the property "underConstruction" exists and is true, return false (don't render features under construction)
+            return feature.properties.underConstruction !== undefined ? !feature.properties.underConstruction : true;
+        }
+        return false;
+    },
+
+    onEachFeature: onEachFeature
+}).addTo(map);
+
+var coorsLayer = L.geoJson(coorsField, {
+    pointToLayer: function (feature, latlng) {
+        return L.marker(latlng, {icon: baseballIcon});
+    },
+
+    onEachFeature: onEachFeature
 }).addTo(map);
