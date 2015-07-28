0c496f72deafed938bc6624c0da145e368e49e22
Jeremiah Lant <jeremiahlant@gmail.com>
2015-07-28 10:50:00 -0400

    nice working map with clustering of usgs gages and multiple baselayers

--- javascript/hello-leaflet/delaware-river-basin-example/js/map.js
+++ javascript/hello-leaflet/delaware-river-basin-example/js/map.js
@@ -1,7 +1,7 @@
+// create group to hold all the features
 var feature_group = new L.featureGroup([]);
 
-var layerOrder = new Array();
-
+// create basemaps
 var grayscale = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
@@ -14,6 +14,7 @@ var opentopomap = L.tileLayer('http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
 	attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 });
 
+// create map
 var map = L.map('map', {
     center: [40.5, -75],
     zoom: 7,
@@ -38,34 +39,34 @@ function popup_streams(feature, layer) {
 }
 
 function popup_usgsgages(feature, layer) {
-	var popupContent = '<table><tr><th scope="row">Gage</th><td>' + String(feature.properties['gage']) + '</td></tr><tr><th scope="row">State</th><td>' + String(feature.properties['State']) + '</td></tr><tr><th scope="row">Name</th><td>' + String(feature.properties['Name']) + '</td></tr><tr><th scope="row">Latitude</th><td>' + String(feature.properties['LatitudeDD']) + '</td></tr><tr><th scope="row">LongitudeD</th><td>' + String(feature.properties['LongitudeD']) + '</td></tr><tr><th scope="row">HUC8</th><td>' + String(feature.properties['HUC8']) + '</td></tr><tr><th scope="row">DA</th><td>' + String(feature.properties['DA']) + '</td></tr></table>';
+	var popupContent = '<table><tr><th scope="row">Gage</th><td>' + String(feature.properties['gage']) + '</td></tr><tr><th scope="row">State</th><td>' + String(feature.properties['State']) + '</td></tr><tr><th scope="row">Name</th><td>' + String(feature.properties['Name']) + '</td></tr><tr><th scope="row">Latitude</th><td>' + String(feature.properties['LatitudeDD']) + '</td></tr><tr><th scope="row">Longitude</th><td>' + String(feature.properties['LongitudeD']) + '</td></tr><tr><th scope="row">HUC8</th><td>' + String(feature.properties['HUC8']) + '</td></tr><tr><th scope="row">DA</th><td>' + String(feature.properties['DA']) + '</td></tr></table>';
 	layer.bindPopup(popupContent);
 }
 
-
-function doStyledrbbasin(feature) {
+// styles for each feature
+function style_drbbasin(feature) {
 		return {
 			color: '#afb38a',
 			fillColor: '#fdbf6f',
-			weight: 1.3,
+			weight: 2,
 			dashArray: '',
 			opacity: 0.439215686275,
 			fillOpacity: 0.439215686275
 		};
 }
 
-function doStylewaterbasins(feature) {
+function style_waterbasins(feature) {
 		return {
 			color: '#809848',
-			fillColor: '#badd69',
-			weight: 1.3,
+			fillColor: '#088A29',
+			weight: 3,
 			dashArray: '',
 			opacity: 0.486274509804,
 			fillOpacity: 0.486274509804
 		};
 }
 
-function doStylestreams(feature) {
+function style_streams(feature) {
 		return {
 			weight: 1.3,
 			color: '#1f78b4',
@@ -77,17 +78,17 @@ function doStylestreams(feature) {
 
 var drbbasin = new L.geoJson(drbbasin, {
 	onEachFeature: popup_drbbasin,
-	style: doStyledrbbasin
+	style: style_drbbasin
 });
 
 var waterbasins = new L.geoJson(waterbasins, {
 	onEachFeature: popup_waterbasins,
-	style: doStylewaterbasins
+	style: style_waterbasins
 });
 
 var streams = new L.geoJson(streams, {
 	onEachFeature: popup_streams,
-	style: doStylestreams
+	style: style_streams
 });
 
 var usgsgages = new L.geoJson(usgsgages, {
@@ -104,40 +105,17 @@ var usgsgages = new L.geoJson(usgsgages, {
 	}
 });
 
-
-layerOrder[layerOrder.length] = drbbasin;
-
-for (index = 0; index < layerOrder.length; index++) {
-	feature_group.removeLayer(layerOrder[index]);
-    feature_group.addLayer(layerOrder[index]);
-}
-
-layerOrder[layerOrder.length] = waterbasins;
-
-for (index = 0; index < layerOrder.length; index++) {
-	feature_group.removeLayer(layerOrder[index]);
-    feature_group.addLayer(layerOrder[index]);
-}
-
-feature_group.addLayer(waterbasins);
-
-layerOrder[layerOrder.length] = streams;
-
-for (index = 0; index < layerOrder.length; index++) {
-	feature_group.removeLayer(layerOrder[index]);
-    feature_group.addLayer(layerOrder[index]);
-}
-
-feature_group.addLayer(streams);
-
 var cluster_group_usgsgages = new L.MarkerClusterGroup({showCoverageOnHover: false});
+
+// add layers to groups
+feature_group.addLayer(drbbasin);
+feature_group.addLayer(waterbasins);
 
 cluster_group_usgsgages.addLayer(usgsgages);
 
+// show features on map
 cluster_group_usgsgages.addTo(map);
-
 feature_group.addTo(map);
-
 
 var baseLayers = {
 	"Grayscale": grayscale,
@@ -152,8 +130,5 @@ var overlays = {
     "DRB basin": drbbasin
 };
 
-// L.control.layers(baseLayers, {"usgsgages": cluster_group_usgsgages, "streams": streams, "waterbasins": waterbasins, "drbbasin": drbbasin}, {collapsed:false}).addTo(map);
-
-L.control.layers(baseLayers, overlays, {collapsed: true}).addTo(map);
-
+L.control.layers(baseLayers, overlays).addTo(map);
 L.control.scale().addTo(map);
