e3488ad93c1edc5285a693a58a9be886000c5bf5
Jeremiah Lant <jlant@usgs.gov>
2017-04-06 10:35:20 -0400

    added a debug example for fortran code

---
+++ fortran/debug/debug-steps.md
@@ -0,0 +1,7 @@
+#debug-steps
+1. break main (run code until the first executable point)
+2. step (run line by line)
+3. print "variable-name"
+4. where (show the line that the gdb debugger is on
+5. quit
+
---
+++ fortran/debug/sum.f95
@@ -0,0 +1,16 @@
+program sum
+
+      implicit none
+
+      real :: a,b,c
+
+      write(*,*)'Enter first number:'
+      read(*,*)a
+      write(*,*)'Enter second number:'
+      read(*,*)b
+
+      write(*,*)'The sum of the two numbers is:'
+      c = a+b
+      write(*,*)c
+
+end program sum
